da9192e4a045553d116bd9027c3322617d199a71
Tech-A03 <tech-a03@MacBook-Air-9.local>
2018-04-09 15:27:19 +0100

    updated ver

--- animal.rb
+++ animal.rb
@@ -2,6 +2,31 @@
 require_relative './living_organism'
 
 class Animal < LivingOrganism
+	def self.traits
+		puts 'Animals can breathe, eat, drink, speak and procreate'
+	end
 
+	def breathe
+		puts "The mitochondria is the powerhouse of the cell"
+	end
+
+	def eat
+		puts "Nutrition is the mission"
+	end
+
+	def drink
+		puts "H 2 WOAH"
+	end
+
+	def speak
+		puts "squeak"
+	end
+
+	def procreate
+		puts "[REDACTED] Admin rights required"
+	end
 end
 
+# mouse = Animal.new
+# mouse.alive
+# mouse.breathe
